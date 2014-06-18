ca89002e543d6400e3d3f0f0f35a41f4818e53b4
Richmond Watkins <richmondwatkins@mac.com>
2014-06-18 14:40:29 -0500

    leaderboard maps working

--- app/static/js/es6/leaderboard.es6.js
+++ app/static/js/es6/leaderboard.es6.js
@@ -15,35 +15,60 @@
 
 
 function showMaps(){
-  // var guessLoc = $('#guessLoc').attr('data-coords');
   var maps = $('.leader-maps');
-  var ids = [];
-  for(var i = 0; i<maps.length; i++){
-    ids.push($(maps[i]).attr('id'));
+  var allGames = [];
+  for(var i = 0; i< maps.length; i++){
+    var game = {};
+    game.guessLoc = $(maps[i]).attr('data-guess');
+    game.actualLoc = $(maps[i]).attr('data-actual');
+    game.id = $(maps[i]).attr('id');
+    allGames.push(game);
   }
 
-  ids.forEach(i=>{
+  allGames.forEach(g=>{
     var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
     var mapOptions = {
       zoom: 1,
       center: myLatlng
     };
-        leaderMaps = new google.maps.Map(document.getElementById(i), mapOptions);
+      g.guessLoc = g.guessLoc.toString();
+      g.actualLoc = g.actualLoc.toString();
+
+    // var guessLatlng = new google.maps.LatLng(g.guessLoc);
+        g.guessLoc = g.guessLoc.replace('(', '');
+        g.guessLoc = g.guessLoc.replace(')', '').split(',');
+
+        g.actualLoc = g.actualLoc.replace('(', '');
+        g.actualLoc = g.actualLoc.replace(')', '').split(',');
+        console.log(g.actualLoc);
+
+        var guessLoc = new google.maps.LatLng(g.guessLoc[0], g.guessLoc[1]);
+        var actualLoc = new google.maps.LatLng(g.actualLoc[0], g.actualLoc[1]);
+
+        leaderMaps = new google.maps.Map(document.getElementById(g.id), mapOptions);
+        console.log(guessLoc);
+        // var actualLoc = new google.maps.LatLng(g.actualLoc);
+        var markers = new google.maps.Marker({
+          position: guessLoc,
+          map: leaderMaps
+        });
+        drawLine(guessLoc, actualLoc, leaderMaps);
   });
 
-  //
-  //
-  //
-
-  //     locations.forEach(l=>{
-  //       gameMarker = new google.maps.Marker({
-  //          position: l.coords[0],
-  //          map: gameMap
-  //        });
-  //        drawLine(l.coords, gameMap);
-  //      });
-
 }
+
+function drawLine(guessLoc, actualLoc, selectedMap){
+  var flightPath = new google.maps.Polyline({
+    path: [guessLoc, actualLoc],
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+    flightPath.setMap(selectedMap);
+}
+
 
 
 
